739eb4c3826b93ecd7aaa63642006155455a2da2
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-08-30 16:24:46 +0530

    added environments directory back to repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  signUpURL: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=",
+  singInURL:
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=",
+  firebaseKey: "[API_Key]"
+};
