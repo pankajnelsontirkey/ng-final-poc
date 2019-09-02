12eca650706d0045c9dc3c9ab6d9b071ba035edf
Pankaj Nelson Tirkey <pankajnelsontirkey@outlook.com>
2019-09-02 19:10:14 +0530

    Unused code removal

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 
 import { UserModel, LoginModel, CurrentUserModel } from '../shared/models';
